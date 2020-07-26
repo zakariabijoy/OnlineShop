030d9707de9bc991ab926558974a4c4472e1b2fe
zakariabijoy <zakaria@vonome.com>
2020-07-27 01:02:04 +0600

    added  Product Controller  and it's index method for showing all products

---
+++ OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var productList = _db.Products.Include(p => p.ProductTypes).Include(p => p.Tag).ToList();
+            return View(productList);
+        }
+    }
+}
