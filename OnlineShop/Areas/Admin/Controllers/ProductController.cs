c3d4a7664faed9f50d19fb2ebaffead73c714192
zakariabijoy <zakaria@vonome.com>
2020-07-30 03:15:20 +0600

    added create get and post action method of product controller in admin area

--- OnlineShop/Areas/Admin/Controllers/ProductController.cs
+++ OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
+using OnlineShop.Models;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -13,14 +14,41 @@ namespace OnlineShop.Areas.Admin.Controllers
     {
         private readonly ApplicationDbContext _db;
 
+
         public ProductController(ApplicationDbContext db)
         {
             _db = db;
         }
+
+        //Index Get Action Method
         public IActionResult Index()
         {
             var productList = _db.Products.Include(p => p.ProductTypes).Include(p => p.Tag).ToList();
             return View(productList);
         }
+
+
+        // Create Get Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // Create Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Products.AddAsync(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
     }
 }
